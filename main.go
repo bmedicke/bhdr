03c27bcfc348c964afc4cc514b05e5ff2a7b6a56
Benjamin Medicke <bmedicke@gmail.com>
2022-04-24 22:52:20 +0200

    add scaffold

--- main.go
+++ main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"github.com/rivo/tview"
+)
+
 func main() {
+	entityNames := []string{"edison", "hue", "fan"}
+	entities := tview.NewTreeNode("entities")
+	entities.SetReference("json")
+	// entities.SetSelectable(false)
+
+	for _, name := range entityNames {
+		entity := tview.NewTreeNode(name)
+		entity.SetReference("json")
+		entities.AddChild(entity)
+	}
+
+	status := tview.NewTextView()
+	status.SetBorder(true).SetTitle("status")
+
+	ha := tview.NewTreeView()
+	ha.SetBorder(true).SetTitle("home")
+	ha.SetRoot(entities)
+	ha.SetCurrentNode(entities)
+
+	ha.SetSelectedFunc(
+		func(node *tview.TreeNode) {
+		},
+	)
+
+	ha.SetChangedFunc(
+		func(node *tview.TreeNode) {
+			text := node.GetText()
+			ref := node.GetReference().(string)
+			status.SetTitle(text)
+			status.SetText(text + " " + ref)
+		},
+	)
+
+	root := tview.NewFlex()
+	root.SetBorder(true).SetTitle("beholder")
+	root.AddItem(ha, 0, 1, false)
+	root.AddItem(status, 0, 1, false)
+
+	app := tview.NewApplication()
+	app.SetRoot(root, true)
+	app.SetFocus(ha)
+	app.Run()
 }
