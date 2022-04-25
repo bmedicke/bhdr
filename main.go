a816521762853c88d5a5d0068a374af3b306cdcc
Benjamin Medicke <bmedicke@gmail.com>
2022-04-25 13:32:40 +0200

    add navigation, add GetParent()

--- main.go
+++ main.go
@@ -1,50 +1,108 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	entityNames := []string{"edison", "hue", "fan"}
-	entities := tview.NewTreeNode("entities")
-	entities.SetReference("json")
-	// entities.SetSelectable(false)
+	rootNode := tview.NewTreeNode(".")
+	// rootNode.SetSelectable(false)
 
+	haEntities := tview.NewTreeNode("home-assistant")
+
+	// fill haEntities with nodes:
+	entityNames := []string{"edison", "hue", "fan"}
 	for _, name := range entityNames {
 		entity := tview.NewTreeNode(name)
-		entity.SetReference("json")
-		entities.AddChild(entity)
+		entity.SetReference("ref for " + name)
+		haEntities.AddChild(entity)
 	}
 
+	rootNode.AddChild(haEntities)
+	rootNode.AddChild(
+		tview.NewTreeNode("localFunctions").
+			AddChild(tview.NewTreeNode("func0")).
+			AddChild(tview.NewTreeNode("func1")),
+	)
+
 	status := tview.NewTextView()
-	status.SetBorder(true).SetTitle("status")
+	status.SetBorder(true)
 
-	ha := tview.NewTreeView()
-	ha.SetBorder(true).SetTitle("home")
-	ha.SetRoot(entities)
-	ha.SetCurrentNode(entities)
+	switches := tview.NewTreeView()
+	switches.SetBorder(true).SetTitle("switches")
+	switches.SetRoot(rootNode)
+	switches.SetTopLevel(1)
 
-	ha.SetSelectedFunc(
-		func(node *tview.TreeNode) {
-		},
-	)
-
-	ha.SetChangedFunc(
-		func(node *tview.TreeNode) {
-			text := node.GetText()
-			ref := node.GetReference().(string)
-			status.SetTitle(text)
-			status.SetText(text + " " + ref)
-		},
-	)
-
-	root := tview.NewFlex()
-	root.SetBorder(true).SetTitle("B H 🐙 D R")
-	root.AddItem(ha, 0, 1, false)
-	root.AddItem(status, 0, 1, false)
+	layout := tview.NewFlex()
+	layout.SetBorder(true).SetTitle("B H 🐙 D R")
+	layout.AddItem(switches, 0, 1, false)
+	layout.AddItem(status, 0, 1, false)
 
 	app := tview.NewApplication()
-	app.SetRoot(root, true)
-	app.SetFocus(ha)
+	app.SetRoot(layout, true)
+	app.SetFocus(switches)
+
+	switches.SetInputCapture(
+		func(event *tcell.EventKey) *tcell.EventKey {
+			sel := switches.GetCurrentNode()
+			switch event.Rune() {
+			default:
+				break
+			case 'q':
+				app.Stop()
+			case '[':
+				break // TODO: jump to previous node on same level.
+			case ']':
+				break // TODO: jump to next node on same level.
+			case 'h':
+				sel.Collapse()
+			case 'l':
+				sel.Expand()
+			case ';', '\'':
+				status.SetText(string(event.Rune()) + " on " + sel.GetText())
+			case 'p':
+				if parent := GetParent(sel, rootNode); parent != nil {
+					status.SetText("parent: " + parent.GetText())
+				} else {
+					status.SetText("no parent found")
+				}
+			}
+			return event
+		},
+	)
+
+	// press Enter on node:
+	switches.SetSelectedFunc(func(node *tview.TreeNode) {})
+
+	// select node:
+	switches.SetChangedFunc(func(node *tview.TreeNode) {})
+
+	switches.SetCurrentNode(rootNode)
 	app.Run()
+}
+
+// GetParent returns parent or nil if it was not found.
+// https://github.com/rivo/tview/issues/246#issuecomment-471173854
+func GetParent(
+	node, root *tview.TreeNode,
+) *tview.TreeNode {
+	var match *tview.TreeNode
+
+	// walk the tree to find our node (and thus its parent):
+	root.Walk(
+		func(current, parent *tview.TreeNode) bool {
+			// current node found:
+			if current == node {
+				if parent != nil {
+					match = parent
+				}
+				// stop walk:
+				return false
+			}
+			// current node not found, continue walk:
+			return true
+		},
+	)
+	return match
 }
